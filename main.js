627ee4190f82615943765c3e6367f94475226df1
Tanapon <tanapon.tanavejwasin@gmail.com>
2026-02-01 13:25:21 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
 async function start() {
-    const weatherPromis = await fetch("https://api.weather.gov/gridpoints/MPX/46,130/forecast");
+    const weatherPromis = await fetch("https://api.weather.gov/gridpoints/MPX/46,130/forecast")
     const weatherData = await weatherPromis.json()
     const ourTemperature = weatherData.properties.periods[0].temperature
     document.querySelector("#temperature-output").textContent = ourTemperature
@@ -8,9 +11,31 @@ async function start() {
 start()
 
 async function petAreas() {
-    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
-    petsData.forEach(pet => console.log(pet.name));
+    petsData.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+        clone.querySelector("h3").textContent = pet.name
+        clone.querySelector(".pet-description").textContent = pet.description
+        clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+        wrapper.appendChild(clone)
+    });
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petAreas()
+
+function createAgeText(birthYear) {
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - Number(birthYear)
+    console.log(currentYear, Number(birthYear), age)
+    if (age == 0) {
+        return "Less than a year old."
+    } else if (age == 1) {
+        return "1 year old."
+    } else {
+        return `${age} years old.`
+    }
+}
