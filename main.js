cb28fe3c92dd47feaac443da72dba24a829ef4f6
Tanapon <tanapon.tanavejwasin@gmail.com>
2026-02-01 10:55:03 +0700

    update fetch data

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 async function start() {
     const weatherPromis = await fetch("https://api.weather.gov/gridpoints/MPX/46,130/forecast");
-    const weatherData = await weatherPromis.json();
+    const weatherData = await weatherPromis.json()
     const ourTemperature = weatherData.properties.periods[0].temperature
     document.querySelector("#temperature-output").textContent = ourTemperature
 }
 
 start()
+
+async function petAreas() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => console.log(pet.name));
+}
+
+petAreas()
